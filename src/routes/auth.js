e1acff7e5482c26ac447b6622d5b37af19da577c
Geovany Gonzalaez <geovanygonzalezdev@gmail.com>
2024-02-08 20:53:46 -0600

    cambio first por unique

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,7 +9,7 @@ router.post("/login", async (req, res) => {
     const { CorreoElectronico, Contrasena } = req.body;
 
     try {
-        const usuario = await prisma.usuarios.findUnique({
+        const usuario = await prisma.usuarios.findFirst({
             where: {
                 CorreoElectronico: CorreoElectronico
             }
