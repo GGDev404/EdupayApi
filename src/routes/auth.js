af15a7631de4cf076f371ee6eea24dfc305d79cc
Geovany Gonzalaez <geovanygonzalezdev@gmail.com>
2024-02-08 20:27:28 -0600

    login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,27 +5,32 @@ import { encrypt, compare } from '../helpper/handleBcryps.js';
 const router = Router();
 const prisma = new PrismaClient();
 
-router.post("/login", async (req, res)=>{
-    const {CorreoElectronico, Contrasena} = req.body
-    const usuario = await prisma.usuarios.findFirst({
-        where: {
-            CorreoElectronico : CorreoElectronico
+router.post("/login", async (req, res) => {
+    const { CorreoElectronico, Contrasena } = req.body;
+
+    try {
+        const usuario = await prisma.usuarios.findFirst({
+            where: {
+                CorreoElectronico: CorreoElectronico
+            }
+        });
+
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
         }
-    })
-    if (!usuario) {
-        res.status(404)
-        res.json('Usuario no encotrado')
-    }
 
-    const ConstraseñaVerificada = await compare(Contrasena, usuario.Contrasena )
-    if (ConstraseñaVerificada) {
-       return res.send(usuario.Id)
-    }
+        const contrasenaVerificada = await compare(Contrasena, usuario.Contrasena);
 
-    if (!ConstraseñaVerificada) {
-        res.status(409)
-        return res.send({ error : 'Contraseña incorrecta'})
+        if (contrasenaVerificada) {
+            return res.json({ userId: usuario.Id });
+        } else {
+            return res.status(404).json({ error: 'Contraseña incorrecta' });
+        }
+    } catch (error) {
+        console.error('Error en la autenticación:', error);
+        return res.status(500).json({ error: 'Error interno del servidor' });
     }
-})
+});
+
 
 export default router;
